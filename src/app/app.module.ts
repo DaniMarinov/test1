056f410711e2d39253d37f48016a1f0a4f8e5eac
Danail Marinov <41998471+DaniMarinov@users.noreply.github.com>
2021-07-08 15:01:18 +0300

    My New App initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { IgxIconModule, IgxAvatarModule, IgxButtonModule, IgxListModule, IgxSliderModule, IgxInputGroupModule, IgxSelectModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { MyInfoComponent } from './my-info/my-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MyInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxIconModule,
+    IgxAvatarModule,
+    IgxButtonModule,
+    IgxListModule,
+    IgxSliderModule,
+    FormsModule,
+    IgxInputGroupModule,
+    IgxSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
